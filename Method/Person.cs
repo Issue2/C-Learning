1f4b5f00cccc1c624d8787dd9620c007848eae83
X <X@DESKTOP-7EUMVCL>
2017-04-05 07:09:40 +0200

    Add project files.

---
+++ Method/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method
+{
+    class Person
+    {
+        public string GetFullname(string Firstname, string Lastname)
+        {
+            return $"The fullname of the person is {Firstname} {Lastname}";
+        }
+    }
+}
---
+++ Method/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            var fullname = person.GetFullname("Avetis", "Ghukasyan");
+
+            Console.WriteLine(fullname);
+        }
+    }
+}
